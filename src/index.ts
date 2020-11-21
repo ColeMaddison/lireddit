5ef54b4a06b38203297942b06da733cffb478484
Dmitry <dmitry@MacBook-Pro-Dmitry.local>
2020-11-21 15:49:24 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import "reflect-metadata";
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__, __port__ } from "./constants";
+import mikroConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express"; 
+import { buildSchema } from "type-graphql";
+import { Hello } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
+
+const main = async () => {
+    const orm = await MikroORM.init(mikroConfig);
+    orm.getMigrator().up();
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [Hello, PostResolver],
+            validate: false,
+        }),
+        context: () => ({ em: orm.em })
+    });
+
+    apolloServer.applyMiddleware({ app });
+
+    app.listen(__port__, () => {
+        console.log(`Server is up and running on port:: ${__port__}`);
+    });
+};
+ 
+main().catch(e => {
+    console.error(e);
+});
---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,19 @@
+import { Post } from "./entities/Post";
+import { __prod__ } from "./constants";
+import { MikroORM } from "@mikro-orm/core";
+import path  from "path";
+
+export default {
+    entities: [Post],
+    migrations: {
+        path: path.join(__dirname, './migrations'), // path to the folder with migrations
+        pattern: /^[\w-]+\d+\.[tj]s$/,
+    },
+    dbName: 'postgres',
+    user: "postgres",
+    password: "postgres",
+    type: 'postgresql',
+    host: 'localhost',
+    port: 25432,
+    debug: !__prod__,
+} as Parameters<typeof MikroORM.init>[0];
